56b8036167b4505444f76cd700c418c54f7e23ca
RmNLcN06 <romain.luciano@orange.fr>
2025-02-01 18:02:25 +0100

    Create Thymeleaf template

--- src/main/java/com/rmnlcn/Spring_MVC/controllers/DemoController.java
+++ src/main/java/com/rmnlcn/Spring_MVC/controllers/DemoController.java
@@ -10,7 +10,7 @@ public class DemoController {
     // create a mapping for "/hello"
     @GetMapping("/hello")
     public String sayHello(Model theModel) {
-        theModel.addAttribute("TheDate", java.time.LocalDateTime.now());
+        theModel.addAttribute("theDate", java.time.LocalDateTime.now());
 
         return "helloworld";
     }
