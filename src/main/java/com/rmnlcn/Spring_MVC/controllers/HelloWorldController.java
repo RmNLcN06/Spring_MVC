6e2940b1428ba524883f533bdb96bcb8d3cafc46
RmNLcN06 <romain.luciano@orange.fr>
2025-02-02 22:06:06 +0100

    Add processFormUpperCase method in HelloWorldController

--- src/main/java/com/rmnlcn/Spring_MVC/controllers/HelloWorldController.java
+++ src/main/java/com/rmnlcn/Spring_MVC/controllers/HelloWorldController.java
@@ -1,6 +1,8 @@
 package com.rmnlcn.Spring_MVC.controllers;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -16,6 +18,25 @@ public class HelloWorldController {
     @RequestMapping("/processForm")
     public String processForm() {
         return "helloworld";
+    }
+
+    // need controller method to read form data & add data to the model
+    @RequestMapping("/processFormUpperCase")
+    public String processFormUpperCase(HttpServletRequest request, Model model) {
+
+        // read the request parameter from the HTML form
+        String theName = request.getParameter("studentName");
+
+        // convert the data to all caps
+        theName = theName.toUpperCase();
+
+        // create the message
+        String result = "Welcome to the team " + theName;
+
+        // add message to the model
+        model.addAttribute("message", result);
+
+        return "helloword";
     }
 
 }
