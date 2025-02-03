0d77ee3db468e25027dd31699d5c839705e574db
RmNLcN06 <romain.luciano@orange.fr>
2025-02-03 11:36:24 +0100

    Create form processing code

--- src/main/java/com/rmnlcn/Spring_MVC/controllers/StudentController.java
+++ src/main/java/com/rmnlcn/Spring_MVC/controllers/StudentController.java
@@ -4,6 +4,8 @@ import com.rmnlcn.Spring_MVC.models.Student;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class StudentController {
@@ -19,5 +21,14 @@ public class StudentController {
         theModel.addAttribute("student", theStudent);
 
         return "student-form";
+    }
+
+    @PostMapping("/processStudentForm")
+    public String processStudentForm(@ModelAttribute("student") Student theStudent) {
+
+        // log the input data
+        System.out.println("theStudent: " + theStudent.getFirstName() + " " + theStudent.getLastName());
+
+        return "student-confirmation";
     }
 }
