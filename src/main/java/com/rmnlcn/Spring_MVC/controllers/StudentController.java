876a6cd57bd36b456c276b656214ac0e0b1fe877
RmNLcN06 <romain.luciano@orange.fr>
2025-02-05 22:03:46 +0100

    Resolve whitespace issue using StringTrimmerEditor & @InitBinder

--- src/main/java/com/rmnlcn/Spring_MVC/controllers/StudentController.java
+++ src/main/java/com/rmnlcn/Spring_MVC/controllers/StudentController.java
@@ -3,10 +3,13 @@ package com.rmnlcn.Spring_MVC.controllers;
 import com.rmnlcn.Spring_MVC.models.Student;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -14,6 +17,15 @@ import java.util.List;
 
 @Controller
 public class StudentController {
+
+    // add initbinder
+    // remove leading and trailing whitespace
+    // resolve issue for our validation
+    @InitBinder
+    public void initBinder(WebDataBinder dataBinder) {
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+        dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+    }
 
     @Value("${countries}")
     private List<String> countries;
