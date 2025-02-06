57be1322898b91e79ef71886bc698600e5ba9936
RmNLcN06 <romain.luciano@orange.fr>
2025-02-06 11:51:15 +0100

    Add validation rule to Student class

--- src/main/java/com/rmnlcn/Spring_MVC/models/Student.java
+++ src/main/java/com/rmnlcn/Spring_MVC/models/Student.java
@@ -1,5 +1,7 @@
 package com.rmnlcn.Spring_MVC.models;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
@@ -16,6 +18,8 @@ public class Student {
     @Size(min = 3, message = "Last name is required and must have 3 letters min")
     private String lastName;
 
+    @Min(value = 17, message = "must be greater than or equal to 17")
+    @Max(value = 35, message = "must be less than or equal to 35")
     private int age;
 
     private String country;
